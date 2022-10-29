ced38dc3ef3652ae8c018dcd7b5e379018da9d70
USER <USER@DESKTOP-5PQG68E>
2022-10-29 09:54:30 +0530

    Third commit extentreport,pom.xml,sreenshot

--- com.supermarket/src/main/java/com/supermarket/constants/Constants.java
+++ com.supermarket/src/main/java/com/supermarket/constants/Constants.java
@@ -3,18 +3,25 @@ package com.supermarket.constants;
 public class Constants {
 public static final String CONFIGURE_FILE_PATH=System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties";
 
-
-
-
 public static final String PDF_FILE_PATH="file:\\"+System.getProperty("user.dir")+"\\src\\main\\resources\\PdfFiles";
 	
 /**Login page**/
 public static final String EXPECTED_ALERT_TEXT="Alert!";
 
 /**Sub-Manage Expense**/
-//public static final String USERFILE_UPLOADOFSUBMANAGEEXPENSE=System.getProperty("user.dir")+"\\com.supermarket\\FileUpload.pdf";
-public static final String USERFILE_UPLOADOFSUBMANAGEEXPENSE="C:\\Users\\USER\\eclipse-workspace\\com.supermarket\\FileUpload\\Grocerylist.pdf";
+//public static final String USERFILE_UPLOADOFSUBMANAGEEXPENSE=System.getProperty("user.dir")+"\\com.supermarket\\FileUpload\\GrocerylList.pdf";
+public static final String USERFILE_UPLOADOFSUBMANAGEEXPENSE="C:\\Users\\USER\\git\\com.supermarket\\com.supermarket\\src\\main\\resources\\FileUpload\\GrocerylList.pdf";
+
+public static final String EXPECTEDRESULT_UPADTEDDATAOFSUBMANAGEEXPENSE="    \r\n"
+		+ "Expense Record Updated Successfully";
 
 
+/**Manage Order**/
+public static final String EXPECTEDTEXT_LISTORDER="List Orders";
 
+/**Manage Product**/
+public static final String IMAGEUPLOAD_MANAGEPRODUCT=System.getProperty("user.dir")+"\\src\\main\\resources\\ImageUpload";
+
+/**Extent report**/
+public static final String EXTENTREPORT_PATH=System.getProperty("user.dir")+"\\extentreport";
 }
---
+++ com.supermarket/src/main/java/com/supermarket/utilities/ScreenShot.java
@@ -0,0 +1,33 @@
+package com.supermarket.utilities;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+
+public class ScreenShot {
+TakesScreenshot takesscreenshot;
+
+public void take_ScreenShot(WebDriver driver,String imageName) {
+	
+	try {
+		
+		takesscreenshot=(TakesScreenshot) driver;// to pick the scrrenshotstep
+		File screenshotfile=takesscreenshot.getScreenshotAs(OutputType.FILE);
+		String timeStamp=new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss").format(new Date());//attach along with image
+		String destination=System.getProperty("user.dir")+"\\ScreenShots\\"+imageName+ " "+timeStamp+".png";
+		File finalNewDestination=new File(destination);
+		FileHandler.copy(screenshotfile, finalNewDestination);
+	}
+	
+	catch(Exception e) {
+		e.printStackTrace();
+	}
+}
+}
